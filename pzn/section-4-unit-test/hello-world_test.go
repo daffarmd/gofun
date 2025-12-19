366f9d003502b032f362214251b2e0f349d0ce13
daffarmd <muhammaddaffarmd@gmail.com>
2025-12-19 09:23:02 +0700

    feat: assert and require test

--- pzn/section-4-unit-test/hello-world_test.go
+++ pzn/section-4-unit-test/hello-world_test.go
@@ -1,6 +1,26 @@
 package section4unittest
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Anis")
+	assert.Equal(t, "Hello, Bambang!", result)
+
+	fmt.Println("Lanjut ke kode ini")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Anis")
+	require.Equal(t, "Hello, Bambang!", result)
+
+	fmt.Println("Tidak lanjut ke kode ini")
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Bambang")
