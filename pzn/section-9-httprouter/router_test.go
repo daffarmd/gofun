8e9ac89680d9ce3c92e7bf3c4c6dd0944769162c
daffarmd <muhammaddaffarmd@gmail.com>
2026-02-21 12:57:30 +0700

    feat: http router

---
+++ pzn/section-9-httprouter/router_test.go
@@ -0,0 +1,29 @@
+package section9httprouter
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHttpRouter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprint(w, "Hello Get")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+	fmt.Print(string(body))
+	assert.Equal(t, "Hello Get", string(body))
+}
